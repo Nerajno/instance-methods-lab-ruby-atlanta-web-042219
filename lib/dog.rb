4a0e276ace28bf0b17d2372bc00599f189d79bee
github username <github email address>
2019-05-22 15:21:36 -0400

    round 2 - ground up'

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,12 @@
 class Dog
+
     def bark
-    puts "Woof!"
+     puts "Woof!"
     end
 
     def sit
-    puts "The Dog is sitting"
+        puts "The Dog is sitting"
     end
+    
 end
 
-class Person
-    def talk
-        puts "Hello World!"
-    end
-
-    def walk
-        puts "The Person is walking"
-    end
-end
